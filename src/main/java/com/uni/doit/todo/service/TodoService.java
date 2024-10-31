e851f5cb0aee07cf52f4ddf66a4fd4ec16c8dd71
hapje010 <hapje01@gmail.com>
2024-10-31 22:37:09 +0900

    tomcat 배포 에러 수정

--- src/main/java/com/uni/doit/todo/service/TodoService.java
+++ src/main/java/com/uni/doit/todo/service/TodoService.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import com.uni.doit.framework.utils.BaseService;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TodoService extends BaseService {
 	
 	// Todo List 
